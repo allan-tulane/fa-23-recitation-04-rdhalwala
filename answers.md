19c767845e2122dcd04fff30ffbb8e0cf31e7031
rdhalwala <126441959+rdhalwala@users.noreply.github.com>
2023-10-24 21:47:09 +0000

    Lab 4

--- answers.md
+++ answers.md
@@ -1,7 +1,7 @@
 # CMPS 2200 Reciation 5
 ## Answers
 
-**Name:**_________________________
+**Name:**___Raiya Dhalwala______________________
 
 
 Place all written answers from `recitation-05.md` here for easier grading.
@@ -14,7 +14,22 @@ Place all written answers from `recitation-05.md` here for easier grading.
 
 - **1b.**
 
+The running time for fixed pivot quicksort was almsot linear, so comparatively terrible compared to the theoretical quicksort runtime. The random pivot runtime was much better than the fixed pivot runtime, but was still close to the worst case asymptotically as it was exponential. As the list size grew, so did the runtime for all functions. 
 
+|      n |   qsort-fixed-pivot |   qsort-random-pivot |   tim_sort |
+|--------|---------------------|----------------------|------------|
+|    100 |               0.157 |                0.285 |      0.011 |
+|    200 |               0.340 |                0.408 |      0.018 |
+|    500 |               1.055 |                1.171 |      0.049 |
+|   1000 |               3.443 |                3.100 |      0.105 |
+|   2000 |              18.769 |                9.644 |      0.246 |
+|   5000 |              46.303 |               27.486 |      0.670 |
+|  10000 |              27.152 |               69.580 |      1.709 |
+|  20000 |             106.728 |              167.249 |      4.313 |
+|  50000 |             267.062 |              465.985 |      8.710 |
+| 100000 |             702.096 |              778.210 |     25.068 |
 
 
 - **1c.**
+
+tim_sort was much faster than quicksort. It was better than quicksort random pivot as well.
--- main.py
+++ main.py
@@ -1,13 +1,47 @@
-import random, time
+import random
+import time
 import tabulate
 
 
 def qsort(a, pivot_fn):
-    ## TO DO
-    pass
-    
+  if len(a) <= 1:
+    return a
+  else:
+    pivot = pivot_fn(a)
+    smaller = []
+    larger = []
+    equal = []
+    for x in a:
+      if x < pivot:
+        smaller.append(x)
+      elif x > pivot:
+        larger.append(x)
+      else:
+        equal.append(x)
+    return qsort(smaller, pivot_fn) + equal + qsort(larger, pivot_fn)
+
+
+#make a random pivot function
+def random_pivot(a):
+  return random.choice(a)
+
+
+#first element is pivot
+def first_pivot(a):
+  return a[0]
+
+
+#selection sort from notes
+def selection_sort(L):
+  for i in range(len(L)):
+    # print(L)
+    m = L.index(min(L[i:]))
+    L[i], L[m] = L[m], L[i]
+  return L
+
+
 def time_search(sort_fn, mylist):
-    """
+  """
     Return the number of milliseconds to run this
     sort function on this list.
 
@@ -24,48 +58,87 @@ def time_search(sort_fn, mylist):
       the number of milliseconds it takes to run this
       search function on this input.
     """
-    start = time.time()
-    sort_fn(mylist)
-    return (time.time() - start) * 1000
-    ###
+  start = time.time()
+  sort_fn(mylist)
+  return (time.time() - start) * 1000
+  ###
 
-def compare_sort(sizes=[100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]):
-    """
+
+#replit told me to make functions, couldnt use lambda
+def qsort_fixed_pivot(a):
+  return qsort(a, first_pivot)
+
+
+def qsort_random_pivot(a):
+  return qsort(a, random_pivot)
+
+
+def tim_sort(a):
+  return sorted(a)
+
+
+def ssort(a):
+  return selection_sort(a)
+
+
+def compare_sort(
+    sizes=[100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]):
+  #WTF DO I DO WITH TIM
+  #tim_sort =  lambda a: sorted(a)
+  #first element pivot
+  #qsort_fixed_pivot = lambda a: qsort(a, first_pivot)
+  #random pivot
+  #qsort_random_pivot = lambda a: qsort(a, random_pivot)
+  #selection sort with array a
+  #ssort = lambda a: selection_sort(a)
+
+  #resutls
+  result = []
+
+  #iterat through the different sizes
+  for size in sizes:
+    # create list in ascending order
+    random_list = list(range(size))
+    # random_list = list(mylist)
+    random.shuffle(random_list)
+
+    result.append([
+        size,
+        time_search(qsort_fixed_pivot, random_list),
+        time_search(qsort_random_pivot, random_list),
+        # time_search(ssort, random_list),
+        time_search(tim_sort, random_list)
+    ])
+    # if size == 50000: break
+
+  return result
+  ###
+  """
     Compare the running time of different sorting algorithms.
 
     Returns:
+    This makes no sense why does it say linear and binary search??
       A list of tuples of the form
       (n, linear_search_time, binary_search_time)
       indicating the number of milliseconds it takes
       for each method to run on each value of n
     """
-    ### TODO - sorting algorithms for comparison
-    qsort_fixed_pivot = # 
-    qsort_random_pivot = #
-    tim_sort = #
-    result = []
-    for size in sizes:
-        # create list in ascending order
-        mylist = list(range(size))
-        # shuffles list if needed
-        #random.shuffle(mylist)
-        result.append([
-            len(mylist),
-            time_search(qsort_fixed_pivot, mylist),
-            time_search(qsort_random_pivot, mylist),
-        ])
-    return result
-    ###
+  ### TODO - sorting algorithms for comparison
+
 
 def print_results(results):
-    """ change as needed for comparisons """
-    print(tabulate.tabulate(results,
-                            headers=['n', 'qsort-fixed-pivot', 'qsort-random-pivot'],
-                            floatfmt=".3f",
-                            tablefmt="github"))
+  """ change as needed for comparisons """
+  print(
+      tabulate.tabulate(
+          results,
+          headers=['n', 'qsort-fixed-pivot', 'qsort-random-pivot', 'tim_sort'],
+          floatfmt=".3f",
+          tablefmt="github"))
+
 
 def test_print():
-    print_results(compare_sort())
+  print_results(compare_sort())
+
 
 random.seed()
 test_print()
